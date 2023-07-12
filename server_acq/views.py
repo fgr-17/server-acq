3fe1e2ad21c117227365f53374c6eba1fb7b5a12
Federico Roux <rouxfederico@gmail.com>
2023-07-12 22:17:15 +0000

    /about path ok with json output

--- server_acq/views.py
+++ server_acq/views.py
@@ -1,7 +1,18 @@
-from flask import render_template
+from flask import Flask, jsonify
 from server_acq import app
 
 @app.route('/')
 def home():
-    return 'hola mundo'
+    return jsonify({"message": "Main dashboard"})
     # return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return jsonify({"message" : "About page"})
+
+# @app.route('/sensors')
+# def about():
+#     return jsonify({"sensors classes" : [{"name" : "level"},
+#                                          {"name" : "height"},
+#                                          {"name" : "chlorine"}]
+#                                          })
